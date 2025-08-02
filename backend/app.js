b872a55508ec10d55d2e5cedda098b2537ae8c52
Amruth M S <msamruth74@gmail.com>
2025-08-02 15:03:47 +0530

    Add app.js with product management routes and server setup

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productmodel from "./models/productmodel.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.post("/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new productmodel(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await productmodel.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
+});
+
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
