a89675aaeda8041540c57e512a9182fc9f9716b6
Amruth M S <msamruth74@gmail.com>
2025-08-02 18:57:50 +0530

    Refactor product routes for consistency and remove redundant API prefix

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+
+
 app.get("/products", async (req, res) => {
   const products = await productmodel.find({});
   try {
@@ -17,17 +19,19 @@ app.get("/products", async (req, res) => {
   }
 });
 
+
+
 app.post("/products", async (req, res) => {
   const product = req.body; // user will send this data
-
   if (!product.name || !product.price || !product.image) {
+    
     return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
+    .status(400)
+    .json({ success: false, message: "Please provide all fields" });
   }
-
+  
   const newProduct = new productmodel(product);
-
+  
   try {
     await newProduct.save();
     res.status(201).json({ success: true, data: newProduct });
@@ -37,9 +41,11 @@ app.post("/products", async (req, res) => {
   }
 });
 
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
 
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  
   try {
     await productmodel.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted" });
@@ -47,6 +53,22 @@ app.delete("/api/products/:id", async (req, res) => {
     res.status(404).json({ success: false, message: "Product not found" });
   }
 });
+
+
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body; 
+  
+  try {
+    const updatedproduct=await productmodel.findByIdAndUpdate(id,product,{new:true});
+    res.status(200).json({ success: true, data:updatedproduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "" });
+  }
+});
+
+
 
 app.listen(5000, () => {
   connectDB();
